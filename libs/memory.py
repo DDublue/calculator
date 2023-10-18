2628abec48f3471da85a534eff1b1123fefdd5f0
David <darwu@ucsc.edu>
2023-10-18 02:03:15 -0700

    added Memory class w/possible methods

--- libs/memory.py
+++ libs/memory.py
@@ -5,10 +5,37 @@
 import pygame
 from pygame.locals import *
 
-def Memory():
+class Memory(object):
     """
     Memory class that stores the information for the calculator.
     
     This includes the answer and a user-saved number.
     """
-    pass
+    def __init__(self):
+        self.answer = 0
+        self.cache = 0
+        
+    def get_answer(self):
+        return self.answer
+    
+    def get_cache(self):
+        return self.cachce
+    
+    def set_answer(self, ans):
+        self.answer = ans
+        
+    def set_cache(self, cache):
+        self.cache = cache
+        
+    def set_cache_add(self, cache):
+        self.cache += cache
+        
+    def set_cache_sub(self, cache):
+        self.cache -= cache
+        
+    def clear_answer(self):
+        self.answer = 0
+        
+    def clear_cache(self):
+        self.cache = 0
+        
