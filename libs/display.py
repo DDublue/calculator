45f89df9fdfc800e0e7f0b2a743fbf173064dc77
David Wu <theonly1living@gmail.com>
2023-10-17 21:46:28 -0700

    initial

---
+++ libs/display.py
@@ -0,0 +1,18 @@
+# display.py
+# Display output for the calculator
+# ---------------------------------------------------------------------
+
+import pygame as pg
+from pygame.locals import *
+from .color import BLACK, WHITE
+
+
+class Display(object):
+    """
+    Display class that shows outputs
+    
+    width: int
+    height: int
+    """
+    def __init__(self, width: int, height: int):
+        pass
