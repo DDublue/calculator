04c33692fdaf5000355bf2f67183db63d134cfc7
David <darwu@ucsc.edu>
2023-10-18 02:03:26 -0700

    added init

--- libs/button.py
+++ libs/button.py
@@ -4,14 +4,16 @@
 
 import pygame as pg
 from pygame.locals import *
+from .color import *
 
 
 class Button(object):
     """
     Button GUI
     """
-    def __init__(self):
-        pass
+    def __init__(self, x, y, width, height, bgcolor=ORANGE):
+        self.surface = pg.Surface((width, height))
+        self.rect = self.surface.get_rect()
     
     def update(self):
         pass
