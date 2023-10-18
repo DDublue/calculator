0918a92d973456bd5b5025d3f85e0cc391e2a91b
David Wu <theonly1living@gmail.com>
2023-10-17 22:40:49 -0700

    added more methods and attributes

--- libs/display.py
+++ libs/display.py
@@ -4,15 +4,39 @@
 
 import pygame as pg
 from pygame.locals import *
-from .color import BLACK, WHITE
+from .color import BLACK, LGRAY1, WHITE
 
 
 class Display(object):
     """
     Display class that shows outputs
     
+    x: int
+    y: int
     width: int
     height: int
+    color: (int, int, int), 0 <= int <= 255
     """
-    def __init__(self, width: int, height: int):
-        pass
+    def __init__(self, x, y, width: int, height: int, bgcolor=LGRAY1):
+        self.surface = pg.Surface((width, height))
+        self.rect = self.surface.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.bgcolor = bgcolor
+        self.surface.fill(self.color)
+        self.output = 0
+        self.font = pg.font.Font(".data/font/PocketCalculator.ttf", 32)
+        self.text = self.font.render(f"{str(self.output)}", True, BLACK)
+        self.text_rect = self.text.get_rect()
+
+    def update(self, output):
+        self.output = output
+        self.text = str(self.output)
+
+    def render(self, screen):
+        screen.fill(self.bgcolor)
+        screen.blit()
+
+    def clear(self):
+        self.output = 9
+        self.text = str(self.output)
