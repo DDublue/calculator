f723e4e31517bdd1b9899bcb3f82cb761918515d
David <darwu@ucsc.edu>
2023-10-18 02:03:45 -0700

    added working display

--- libs/main.py
+++ libs/main.py
@@ -5,10 +5,10 @@
 import pygame as pg
 from pygame.locals import *
 
-from libs.button import Button
-from libs.color import BLACK, WHITE
-from libs.display import Display
-from libs.memory import Memory
+from .button import Button
+from .color import BLACK, WHITE
+from .display import Display
+from .memory import Memory
 
 
 class main(object):
@@ -20,28 +20,33 @@ class main(object):
         pg.display.set_caption("Calculator")
         self.WIDTH = width
         self.HEIGHT = height
-        self.screen = pg.display.set_mode((self.WIDTH, self.HEIGHT), pg.RESIZABLE)
+        self.screen = pg.display.set_mode((self.WIDTH, self.HEIGHT))
         self.fps = 60
         self.clock = pg.time.Clock()
         self.running = True
+        self.display = Display(x=self.WIDTH//2, y=0,
+                               width=300, height=70)
 
     def event_loop(self):
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 self.running = False
+            elif event.type == pg.KEYDOWN and event.key == K_ESCAPE:
+                self.running = False
         
     def update(self):
-        pass
+        self.display.update()
 
-    def render(self, screen):
+    def render(self):
         self.screen.fill(WHITE)
+        self.display.render(self.screen)
         pg.display.update()
 
     def run(self):
         while self.running:
             self.event_loop()
             self.update()
-            self.render(self.screen)
+            self.render()
             self.clock.tick(self.fps)
         pg.quit()
 
