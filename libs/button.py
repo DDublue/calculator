50c0858559c14e0bcbfc310c19136f7e94c7e489
David <darwu@ucsc.edu>
2023-10-17 23:28:44 -0700

    added Button class

---
+++ libs/button.py
@@ -0,0 +1,20 @@
+# button.py
+# Button GUI
+# ---------------------------------------------------------------------
+
+import pygame as pg
+from pygame.locals import *
+
+
+class Button(object):
+    """
+    Button GUI
+    """
+    def __init__(self):
+        pass
+    
+    def update(self):
+        pass
+    
+    def render(self):
+        pass
